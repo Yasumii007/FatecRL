30c2dbc631406ffa479c4f53e33d9cd990d7e946
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-03-12 18:05:47 -0300

    Commit de atualização de exercicio

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("FATEC Rubens Lara (Baixada Santista)");
+Console.WriteLine("Você sabe o que significa FATEC?");
+Console.WriteLine("FATEC é a sigla para Faculdade de Tecnologia, uma instituição de ensino superior pública estadual, pertencente ao Centro Estadual de Educação Tecnológica Paula Souza");
